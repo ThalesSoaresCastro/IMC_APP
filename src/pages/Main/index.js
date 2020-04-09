836409d26c63c56fcc3c377fbf7bdd54d90ae2cd
thales <th4les.castro@gmail.com>
2020-04-09 01:03:00 -0300

    Criado TDD e Tela de fundo.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,5 +3,12 @@ import React from 'react';
 import {
   View,
 } from 'react-native';
+import { Container } from './styles';
 
+export default function Main(){
+  return(
+    <Container />
+  )
+
+}
 
